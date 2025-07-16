9827455e75f126b67d0c44957bb1b9f45431ec48
OAO-Kamu <xiaolong1145140008@qq.com>
2025-07-17 01:09:55 +0800

    更新 .lua

--- .lua
+++ .lua
@@ -6,16 +6,31 @@
 |     _|                                      
                Made By: NOEKemono-Kamu
 ]]
-local Lib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/BoredStuff2/notify-lib/main/lib'),true))()
+local OrionLib = loadstring(game:HttpGet("https://raw.github.com/XiaoLingUwU/XiaoLing_-_-R_-_-O_-_-B_-_-L_-_-O_-_-X_-UI_-X/main/XiaoLing.UI-Kong-AA.Lua"))()
 getgenv().Games = {
     [9872472334] = "https://raw.github.com/OAO-Kamu/Roblox-Kamu-Evade-CHANGED-Script/refs/heads/main/Evade.Luau",--EVADE PC
-    [124387865885397] = "https://raw.github.com/OAO-Kamu/Main/refs/heads/main/%E8%A2%AB%E9%81%97%E5%BC%83.lua",--Forsaken Mobile
-    [1] = "https://raw.github.com/OAO-Kamu/I/refs/heads/main/I.luau"--anygame
+    [18687417158] = "https://raw.github.com/OAO-Kamu/Main/refs/heads/main/%E8%A2%AB%E9%81%97%E5%BC%83.lua",--Forsaken Mobile
+    [16991287194] = "https://raw.github.com/OAO-Kamu/I/refs/heads/main/S.E.W.H..Luau",--Something Evil Will Happen Mobile
 }
 
 local id = game.PlaceId
 local url = getgenv().Games[id]
 if url then
- Lib.prompt('✅| |成功!', '已找到服务器: '..game.GameId)
+OrionLib:MakeNotification({
+	Name = "成功!",
+	Content = "已找到服务器: "..game.GameId,
+	Image = "rbxassetid://4483345998",
+	Time = 10
+})
     loadstring(game:HttpGet(url))()
+end
+local ulr = "https://raw.github.com/OAO-Kamu/I/refs/heads/main/I.luau"
+if not url then
+OrionLib:MakeNotification({
+	Name = "没有找到支持的服务器",
+	Content = "已自动加载通用...",
+	Image = "rbxassetid://4483345998",
+	Time = 10
+})
+loadstring(game:HttpGet(ulr))()
 end
