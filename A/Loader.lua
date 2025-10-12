00e5e8f1268fe1cf149283bd872ac941800dd311
OAO-Kamu <xiaolong1145140008@qq.com>
2025-10-12 15:54:23 +0800

    更新 Loader.lua

--- A/Loader.lua
+++ A/Loader.lua
@@ -16,4 +16,4 @@
 
 --https://guns.lol/q3e4.dev
 
-loadstring(game:HttpGet("https://raw.github.com/OAO-Kamu/I/main/Open Source Project/Snnn.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OAO-Kamu/I/refs/heads/main/Open%20Source%20Project/Snnn.lua"))()
